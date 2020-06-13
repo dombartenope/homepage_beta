665bfb1ec150057f3bf2718c5cc99d30693e0942
Dominick Bartenope <dominickbartenope@yahoo.com>
2020-06-13 14:50:19 -0400

    Added focus component with nav functionality

--- src/Container.svelte
+++ src/Container.svelte
@@ -3,13 +3,67 @@
   import Analog from "./Analog.svelte";
   import Digital from "./Digital.svelte";
   import Date from "./Date.svelte";
+  import Focus from "./Focus.svelte";
 
+  let clicked = false;
   let analog = true;
+  let input;
+  let focus;
+
+  const handleInput = e => {
+    focus = input;
+    input = "";
+  };
+
   const switchClock = () => {
     analog = !analog;
   };
 </script>
 
+<style>
+  nav {
+    height: 4em;
+  }
+  form {
+    height: 4em;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .focus {
+    padding: 10px;
+    caret-color: rgb(209, 193, 193);
+    text-align: left;
+    font-family: "Poiret One", cursive;
+    font-size: 2em;
+    font-weight: bold;
+    width: 100%;
+    background: transparent;
+    height: 2em;
+    color: #fff;
+  }
+  .focus::placeholder {
+    text-align: center;
+  }
+  .focus:focus {
+    outline: none;
+  }
+</style>
+
+<!-- NAVBAR -->
+<nav class="bg1">
+  <form on:submit|preventDefault={handleInput} autocomplete="off">
+    <input
+      bind:value={input}
+      class="focus"
+      name="focus"
+      placeholder="What's your focus today?"
+      type="text" />
+  </form>
+</nav>
+<!-- GRID CONTAINER -->
 <div class="container">
   <!--WEATHER-->
   <div id="weather" class="bg1">
@@ -28,8 +82,9 @@
     <Date />
   </div>
 
-  <div class="bg1" />
-  <div class="bg1" />
+  <div id="focus" class="bg1">
+    <Focus {focus} />
+  </div>
 
   <div id="todo" class="bg2">TODO LIST</div>
 
